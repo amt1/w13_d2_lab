867d55d1b2ad0d292d010a2dfd9dc9063184480a
ericxcross <eric.cross@sympatico.ca>
2019-05-21 10:08:28 +0100

    file structure added

---
+++ src/main/java/com/codeclan/employeeProjectsLab/EmployeeProjectsLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.employeeProjectsLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeProjectsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeProjectsLabApplication.class, args);
+	}
+
+}
